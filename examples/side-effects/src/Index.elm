352e578c7db7d5d6ccca30a70682bdb2f046ae22
Mats Stijlaart <m.stijlaart@avisi.nl>
2016-01-14 14:56:12 +0100

    Update side effects example

--- examples/side-effects/src/Index.elm
+++ examples/side-effects/src/Index.elm
@@ -1,11 +1,10 @@
-module Main (..) where
+module Index (..) where
 
-import Html exposing (..)
-import Html.Events exposing (onClick)
 import StartApp
-import Effects exposing (Never, Effects)
-import ProjectApi exposing (getProjects, Project)
 import Task
+import Effects exposing (Never)
+import Projects
+import Html
 
 
 main : Signal Html.Html
@@ -18,76 +17,11 @@ port tasks =
     app.tasks
 
 
-app : StartApp.App Model
+app : StartApp.App Projects.Model
 app =
     StartApp.start
-        { init = init
-        , update = update
-        , view = view
+        { init = Projects.init
+        , update = Projects.update
+        , view = Projects.view
         , inputs = []
         }
-
-
-type Action
-    = Increment
-    | Decrement
-    | AllProjects (Maybe (List Project))
-
-
-type alias Model =
-    { counter : Int
-    , projects : Maybe (List Project)
-    , gotAll : Bool
-    }
-
-
-init : ( Model, Effects Action )
-init =
-    ( { counter = 1, projects = Nothing, gotAll = False }
-    , getProjects (\x -> AllProjects x)
-    )
-
-
-view : Signal.Address Action -> Model -> Html.Html
-view address model =
-    div
-        []
-        [ button [ onClick address Decrement ] [ text "-" ]
-        , div [] [ text (toString model) ]
-        , button [ onClick address Increment ] [ text "+" ]
-        , Maybe.withDefault noProjects (Maybe.map projectsList model.projects)
-        ]
-
-
-noProjects : Html.Html
-noProjects =
-    div
-        []
-        [ em [] [ text "We could not load any projects!" ] ]
-
-
-projectsList : List Project -> Html.Html
-projectsList projects =
-    ul
-        []
-        (List.map projectItem projects)
-
-
-projectItem : Project -> Html.Html
-projectItem project =
-    li
-        []
-        [ text (project.name ++ " (" ++ project.key ++ ")") ]
-
-
-update : Action -> Model -> ( Model, Effects Action )
-update action model =
-    case action of
-        Increment ->
-            ( { model | counter = model.counter + 1 }, Effects.none )
-
-        Decrement ->
-            ( { model | counter = model.counter - 1 }, Effects.none )
-
-        AllProjects x ->
-            ( { model | projects = x, gotAll = True }, Effects.none )
---
+++ examples/side-effects/src/Projects.elm
@@ -0,0 +1,126 @@
+module Projects (init, update, view, Model, Action) where
+
+import Effects exposing (Effects)
+import Http
+import Json.Decode as Json exposing ((:=), maybe)
+import Task exposing (Task)
+import Html exposing (..)
+import Html.Attributes exposing (style)
+import Effects exposing (Never, Effects)
+import Result
+
+type Action
+    = FetchedProjects Projects
+
+
+type alias Model =
+    Projects
+
+
+init : ( Model, Effects Action )
+init =
+    ( Loading
+    , Effects.map FetchedProjects getProjects
+    )
+
+
+view : Signal.Address Action -> Model -> Html.Html
+view address model =
+    case model of
+        Loading ->
+            loadingProjects
+
+        Error s ->
+            projectsErrorAlert s
+
+        Success projects ->
+            projectsList projects
+
+
+loadingProjects : Html.Html
+loadingProjects =
+    div [] [ em [] [ text "Loading projects..." ] ]
+
+
+projectsErrorAlert : String -> Html.Html
+projectsErrorAlert s =
+    div
+        [ style [ ( "color", "red" ) ] ]
+        [ em [] [ text ("We could not load projects: " ++ s) ] ]
+
+
+projectsList : List Project -> Html.Html
+projectsList projects =
+    ul
+        []
+        (List.map projectItem projects)
+
+
+projectItem : Project -> Html.Html
+projectItem project =
+    li
+        []
+        [ text (project.name ++ " (" ++ project.key ++ "/"  ++ (toLead project.lead) ++ ")") ]
+
+toLead : Maybe String -> String
+toLead = Maybe.withDefault "UNKNOWN"
+
+update : Action -> Model -> ( Model, Effects Action )
+update action model =
+    case action of
+        FetchedProjects x ->
+            ( x, Effects.none )
+
+
+
+{- Fetch projects -}
+
+getProjects : Effects Projects
+getProjects =
+    delayFetch
+        |> Task.toResult
+        |> Task.map toListOrError
+        |> Effects.task
+
+
+delayFetch : Task Http.Error (List Project)
+delayFetch = Task.andThen (Task.sleep 2000) (\_ -> fetch)
+
+
+toListOrError : Result Http.Error (List Project) -> Projects
+toListOrError x =
+    case x of
+        Err s ->
+            Error (toString s)
+
+        Ok projects ->
+            Success projects
+
+
+fetch : Task Http.Error (List Project)
+fetch =
+    Http.get projects "http://localhost:8000/projects.json"
+
+
+type Projects
+    = Loading
+    | Error String
+    | Success (List Project)
+
+
+type alias Project =
+    { key : String, name : String, lead : Maybe String }
+
+
+projects : Json.Decoder (List Project)
+projects =
+    Json.list project
+
+
+project : Json.Decoder Project
+project =
+    Json.object3
+        Project
+        ("key" := Json.string)
+        ("name" := Json.string)
+        (maybe ("lead" := Json.string))
